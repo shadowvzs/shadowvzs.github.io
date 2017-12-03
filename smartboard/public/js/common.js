a3007ac761c6e83185f530ea38b3895127367349
shadowvzs <shadowvzs@hotmail.com>
2017-12-03 10:03:56 +0200

    Responsive, Front-end blog model

--- smartboard/public/js/common.js
+++ smartboard/public/js/common.js
@@ -3,10 +3,8 @@ var core = {
 	getDate (timestamp = "0"){
 		let t = new Date(parseInt(timestamp, 10)*1000);
 		return `${t.getFullYear()}. ${(t.getMonth()+1).toString().padStart(2, "0")}. 
-			${t.getDate().toString().padStart(2, "0")} 						
-			${t.getHours().toString().padStart(2, "0")}:
-			${t.getMinutes().toString().padStart(2, "0")}:
-			${t.getSeconds().toString().padStart(2, "0")}`;
+			${t.getDate().toString().padStart(2, "0")} &nbsp;&nbsp;&nbsp;					
+			${t.getHours().toString().padStart(2, "0")}:${t.getMinutes().toString().padStart(2, "0")}:${t.getSeconds().toString().padStart(2, "0")}`;
 	},
 	userVerification() {
 		$("header .login a#logIn").fadeIn();
@@ -18,17 +16,28 @@ var core = {
 			
 			$("header .login a#logIn").hide();
 			$("header .login a#logOut").fadeIn();
+			core["userId"] = key;
 		}
 	},
 	logout() {
 		localStorage.removeItem("user");
 		location.reload(); 
 	},
+	errorMsg(msg){
+		windowManager.newDialog(2, msg, [["Close", "close", false, false]]);
+	},
+	infoMsg(msg){
+		windowManager.newDialog(0, msg, [["Close", "close", false, false]]);
+	},	
+	timedMsg(msg, sec){
+		windowManager.newMessage(msg, [[sec, "close", false, false]])
+	},
 	systemInit( ){
-		localStorage.DB ? (core.loadDatabase(JSON.parse(localStorage.DB)),core.userVerification()) : $.getJSON("./database/db.json", result => {core.loadDatabase(result);core.userVerification()})
+		localStorage.DB ? (core.loadDatabase(JSON.parse(localStorage.DB)), core.userVerification()) : $.getJSON("./database/db.json", result => {core.loadDatabase(result);core.userVerification()})
 	}
 };
 
 $(document).ready( () => {
 	core.systemInit();
+	win = new windowBuilder;
 });			
