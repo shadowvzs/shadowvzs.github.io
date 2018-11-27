c013e709c6a6de9d863c5f5b0c68f010ba6df940
Zsolt <shadowvzs@hotmail.com>
2018-11-27 19:39:32 +0200

    test

--- arcade/js/MobilHelper.js
+++ arcade/js/MobilHelper.js
@@ -22,7 +22,7 @@ class MobilHelper {
             }
 
             let direction;
-alert(dX+' '+dY+' '+Math.abs(dX)+' '+Math.abs(dY)+' '+(Math.abs(dX) > Math.abs(dX));
+alert(dX+' '+dY+' '+Math.abs(dX)+' '+Math.abs(dY)+' '+(Math.abs(dX) > Math.abs(dX)));
             if (Math.abs(dX) > Math.abs(dX)) {
                 direction = dX > 0 ? 'right' : 'left';
             } else {
